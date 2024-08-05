74a5a6435a2570ef2b130c6926376192faf58ecf
Hendrius Félix <hendriusfelix@gmail.com>
2024-08-04 22:22:25 -0300

    funncionamento de conexão e inserção com banco de dados

--- server/server.ino
+++ server/server.ino
@@ -1,33 +1,61 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <SimplePgSQL.h>
+#include <Ultrasonic.h>
+#include <cmath>
 
-const char *ssid = "DASS-CORP";
-const char *pass = "dass7425corp";
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+// Configuração do sensor
+#define TRIGGER_PIN  13
+#define ECHO_PIN     5 
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+// Configuração da rede WiFi
+const char *ssid = "Drogo";
+const char *pass = "75982466703";
 WiFiClient client;
 
-// Setup connection and Database
-IPAddress PGIP(192, 168, 26, 90);
+// Configuração do banco de dados
+IPAddress PGIP(192, 168, 1, 17);
 const char user[] = "postgres";
-const char password[] = "gdti5s11se";
+const char password[] = "wa0i4Ochu";
 const char dbname[] = "sest";
-const int pgPort = 5432; // Porta padrão para PostgreSQL
-
+const int pgPort = 5432;
 PGconnection conn(&client, 0, 1024, nullptr);
-
 char inbuf[128];
 int pg_status = 0;
 
-// Set to read every 60 sec
+// Intervalo de leitura (10 segundos)
 unsigned long previousMillis = 0;
-const long interval = 60000;
+const long interval = 20000;
 
-const int sensorPin = 4;
-int sensorState = 0;
+
+float measureVolume(float distance_from_sensor, float r) {
+  // Dados do cilindro
+  float h_total = 22.3; // Altura total do cilindro em centímetros
+  
+  // Calcula a altura preenchida
+  float h_filled = h_total - distance_from_sensor;
+
+  // Calcula o volume atual preenchido
+  float v_filled = M_PI * pow(r, 2) * h_filled;
+
+  float v_total = M_PI * pow(r, 2) * h_total;
+
+  return v_filled; // Volume preenchido em cm³
+}
+
+
 
 void setup() {
   Serial.begin(115200);
-  pinMode(sensorPin, INPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  //pinMode(LED_BUILTIN, OUTPUT);
 
   WiFi.begin(ssid, pass);
 
@@ -37,13 +65,14 @@ void setup() {
     Serial.println(ssid);
   }
   Serial.println("Connected to WiFi");
-  Serial.println("Timer set to 60 seconds.");
+  Serial.println("Timer set to 10 seconds.");
 }
 
 void doPg(void) {
+  char *msg;
   int rc;
 
-  if (pg_status == 0) {
+  if (!pg_status) {
     Serial.println("Attempting connection...");
 
     // Configuração da conexão com PostgreSQL
@@ -53,68 +82,125 @@ void doPg(void) {
   }
 
   if (pg_status == 1) {
-    Serial.print("Connection status: ");
     rc = conn.status();
+    Serial.print("Connection status: ");
+    Serial.println(pg_status);
+
     if (rc == CONNECTION_BAD || rc == CONNECTION_NEEDED) {
+      char *c = conn.getMessage();
+      if (c) Serial.println(c);
+      pg_status = -1;
       Serial.print("Connection error: ");
       Serial.println(conn.getMessage());
-      pg_status = -1;
-      return;
     } else if (rc == CONNECTION_OK) {
       pg_status = 2;
       Serial.println("Connection established.");
+      Serial.println("Starting query");
     }
     return;
   }
 
   if (pg_status == 2 && strlen(inbuf) > 0) {
-    if (conn.execute(inbuf)) {
-      Serial.println("Error executing query.");
-      pg_status = -1;
-      memset(inbuf, 0, sizeof(inbuf));
-      return;
-    }
-    Serial.println("Query executed.");
+    if (conn.execute(inbuf)) goto error;
+    Serial.println("Working...");
     pg_status = 3;
     memset(inbuf, 0, sizeof(inbuf));
   }
 
   if (pg_status == 3) {
     rc = conn.getData();
-    if (rc < 0) {
-      Serial.println("Error getting data.");
-      pg_status = -1;
-      return;
+    int i;
+    if (rc < 0) goto error;
+    if (!rc) return;
+    if (rc & PG_RSTAT_HAVE_COLUMNS) {
+      for (i = 0; i < conn.nfields(); i++) {
+        if (i) Serial.print(" | ");
+        Serial.print(conn.getColumn(i));
+      }
+      Serial.println("\n==========");
     }
-    if (rc == PG_RSTAT_READY) {
+    else if (rc & PG_RSTAT_HAVE_ROW) {
+      for (i = 0; i < conn.nfields(); i++) {
+        if (i) Serial.print(" | ");
+        msg = conn.getValue(i);
+        if (!msg) msg = (char *)"NULL";
+        Serial.print(msg);
+      }
+      Serial.println();
+    }
+    else if (rc & PG_RSTAT_HAVE_SUMMARY) {
+      Serial.print("Rows affected: ");
+      Serial.println(conn.ntuples());
+    }
+    else if (rc & PG_RSTAT_HAVE_MESSAGE) {
+      msg = conn.getMessage();
+      if (msg) Serial.println(msg);
+    }
+    if (rc & PG_RSTAT_READY) {
       pg_status = 2;
-      Serial.println("Waiting for next query.");
+      Serial.println("Waiting query");
     }
+  }
+  return;
+  
+  error:
+  msg = conn.getMessage();
+  if (msg) Serial.println(msg);
+  else Serial.println("UNKNOWN ERROR");
+  if (conn.status() == CONNECTION_BAD) {
+    Serial.println("Connection is bad");
+    pg_status = -1;
   }
 }
 
 void loop() {
+
+  delay(50); 
+  doPg();
+
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    int level_test = 0;
 
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500); 
+    digitalWrite(LED_BUILTIN, LOW);
     if (WiFi.status() == WL_CONNECTED) {
-      sensorState = digitalRead(sensorPin);
 
-      if (sensorState != HIGH) {
-        level_test += 1;
-        Serial.print("Diesel level: ");
-        Serial.println(level_test);
+      digitalWrite(TRIGGER_PIN, LOW);
+      delayMicroseconds(2);
+      digitalWrite(TRIGGER_PIN, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(TRIGGER_PIN, LOW);
 
-        // Prepare SQL query
-        snprintf(inbuf, sizeof(inbuf), "INSERT INTO manutencao.diesel (nivel, unidade_dass) VALUES (%.2f, %d)", level_test, 1);
-      }
+      long duration = pulseIn(ECHO_PIN, HIGH);
+      float distance = duration * 0.0343 / 2;
+      float volume = measureVolume(distance, 3.85);
+
+      if (distance == 0) {
+        Serial.println("Erro no sensor");
+        } else {
+          Serial.print("Distance: ");
+          Serial.print(distance);
+          Serial.println(" cm");
+
+          Serial.println("=============================");
+          
+          Serial.print("Volume: ");
+          Serial.print(volume);
+          Serial.println(" ml");
+        }
+
+      delay(1000);
+      // Prepare SQL query
+      snprintf(inbuf, sizeof(inbuf), "INSERT INTO manutencao.diesel (nivel, unidade_dass) VALUES (%.2f, %d)", volume, 1);   
+
+      
     } else {
       Serial.println("WiFi Disconnected");
     }
   }
-
-  delay(50); // Adjust delay as needed
-  doPg(); // Call doPg() to handle PostgreSQL operations
 }
+
+
+  
